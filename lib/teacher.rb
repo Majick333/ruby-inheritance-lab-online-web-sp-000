f77ff75337ba2c8a28b1008d2b1508923f810fc2
Ryan M <rmccauleymusic@gmail.com>
2019-11-24 17:13:14 +0000

    Automatically backed up by Learn

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -4,17 +4,14 @@ class Teacher < User
   def initialize
     @first_name = first_name
     @last_name = last_name
-    @knowhow
+
   end
 
   KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
 
-  def teach(knowledge)
-    KNOWLEDGE.each do |idea|
-      @knowhow << idea
-    end
-    ans = @knowhow[rand(@knowhow.length)]
-      ans
-    end
+  def teach
+    KNOWLEDGE.sample
+  end
+  
 
 end
